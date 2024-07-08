4a5b374645a0951253aefb1e951bc7742c585100
Nico Robin <luffy@saitama>
2024-07-08 17:20:00 +0530

    second commit

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -4,48 +4,48 @@ import { Link } from "react-router-dom";
 import "./Home.css";
 
 export default function App() {
-  return (
-    <>
-      {/* Display "Home" page content */}
-      <div className="flex-container">
-        <div className="home-container container">
-          <h1 className="home-welcome-text">
-            Welcome to <img src="icon.svg" alt="QKart"></img>
-          </h1>
+    return (
+        <>
+            {/* Display "Home" page content */}
+            <div className="flex-container">
+                <div className="home-container container">
+                    <h1 className="home-welcome-text">
+                        Welcome to <img src="icon.svg" alt="QKart"></img>
+                    </h1>
 
-          <p>Please select an option from below</p>
+                    <p>Please select an option from below</p>
 
-          <div className="home-buttons">
-            <Link to="/register">
-              <Button
-                id="register-button"
-                className="btn-block"
-                type="primary"
-                block={true}
-              >
-                Register
-              </Button>
-            </Link>
+                    <div className="d-flex flex-column justify-content-center flex-sm-row">
+                        <Link to="/register">
+                            <Button
+                                id="register-button"
+                                className="btn-block"
+                                type="primary"
+                                block={true}
+                            >
+                                Register
+                            </Button>
+                        </Link>
 
-            <Link to="/login">
-              <Button
-                id="login-button"
-                className="btn-block"
-                type="primary"
-                block={true}
-              >
-                Login
-              </Button>
-            </Link>
+                        <Link to="/login">
+                            <Button
+                                id="login-button"
+                                className="btn-block"
+                                type="primary"
+                                block={true}
+                            >
+                                Login
+                            </Button>
+                        </Link>
 
-            <Link to="/products">
-              <Button className="btn-block" type="primary" block={true}>
-                Browse Products
-              </Button>
-            </Link>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+                        <Link to="/products">
+                            <Button className="btn-block" type="primary" block={true}>
+                                Browse Products
+                            </Button>
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 }
